ef55ce3096c34c911d64ae92c5e6c778fd812966
809083 <809083@seq.org>
2022-11-01 13:43:57 -0700

    Polishing movement in Flocking
    Tried Reducing values to reduce erratic movement

--- Lab 1024 Flocking/main.js
+++ Lab 1024 Flocking/main.js
@@ -13,7 +13,7 @@ function init(){
 //  animation loop called 60 fps
 function animate(){
     // paint the canvas with mostly transparent black
-    world.ctx.fillStyle = 'rgba(0,0,0,.5)'
+    world.ctx.fillStyle = 'rgba(0,0,0,1)'
     world.ctx.fillRect(0,0,world.canvas.width,world.canvas.height);
     world.run();    // run the world
   requestAnimationFrame(animate);
