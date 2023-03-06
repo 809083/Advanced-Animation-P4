e7f883eeba52c1c9465179f69a0c2942ba844a8a
Spencer Bergland <809083@seq.org>
2023-03-06 11:52:57 -0800

    save

---
+++ ServerTest 32/testserver.js
@@ -0,0 +1,17 @@
+const http = require('node:http');
+
+// Create a local server to receive data from
+const server = http.createServer();
+
+// Listen to the request event
+server.on('request', (request, res) => {
+  //console.log("request", request);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
+});
+
+server.listen(8000);
+
+console.log("Server Started on Port 8000. Press CTRL-C to terminate");
